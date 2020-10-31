bd21739799814891bfc116953187e0551091ca5f
ritwik jha <jha.ritwik.17.9@gmail.com>
2020-11-01 00:02:24 +0530

    Updates

---
+++ precache-manifest.8a529c5eed7be8f278af84c97154c481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca308a91aea6707aefd882dad7d4f65",
+    "url": "/react-tour/index.html"
+  },
+  {
+    "revision": "0adb007678947133fb48",
+    "url": "/react-tour/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/react-tour/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tour/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0adb007678947133fb48",
+    "url": "/react-tour/static/js/main.566eaa4b.chunk.js"
+  },
+  {
+    "revision": "c9aa3daed3e09aebb379",
+    "url": "/react-tour/static/js/runtime-main.6c213bf3.js"
+  }
+]);
